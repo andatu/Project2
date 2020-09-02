8b826fbb262271f0cbf975bfe9cdcf3d1ac8477a
andatu <andre.aturai@outlook.com>
2020-09-02 21:36:14 +0200

    with log

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -2,15 +2,21 @@ package org.example;
 
 import org.apache.commons.lang3.StringEscapeUtils;
 
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.logging.Logger;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
+    static Logger log = Logger.getLogger(App.class.getName());
     public static void main( String[] args )
     {
         String p = "<p>My paragraph</p>";
         System.out.println(StringEscapeUtils.escapeHtml4(p));
+        log.info("Hello this is a info message");
     }
 }
